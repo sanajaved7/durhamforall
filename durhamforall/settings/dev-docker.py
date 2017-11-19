fff74dd2d656ea3362c0796ead4bbc0e9cda0051
Tim Stallmann <tim@stallmann.mayfirst.org>
2017-11-18 23:53:02 -0500

    Update dockerfile to use postgres image

--- durhamforall/settings/dev-docker.py
+++ durhamforall/settings/dev-docker.py
@@ -2,6 +2,21 @@ from __future__ import absolute_import, unicode_literals
 
 from .base import *
 
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django',
+        'USER': 'pivhhupdpzootr',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': '5432',
+    }
+}
+
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
