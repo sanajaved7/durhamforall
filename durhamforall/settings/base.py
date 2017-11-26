e124e99291e1f1b8a44bc0e32f1748cee5bf3891
Tim Stallmann <tim@stallmann.mayfirst.org>
2017-11-26 16:27:52 -0500

    Fix heroku build errors and make minor tweaks
    Attempt to fix heroku build error
    
    Enable logging on heroku
    
    Tweak how static assets are rendered
    
    Switch back static files finder
    
    Tweak menu colors

--- durhamforall/settings/base.py
+++ durhamforall/settings/base.py
@@ -90,6 +90,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'durhamforall.wsgi.application'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
+        },
+    },
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
--- durhamforall/wsgi.py
+++ durhamforall/wsgi.py
@@ -14,7 +14,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "durhamforall.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "durhamforall.settings.production")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
